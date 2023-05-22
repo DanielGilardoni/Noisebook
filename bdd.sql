719ddb8ada5302d5c13cdc15c1554783636de671
Daniel Gilardoni <dany.gilardoni@gmail.com>
2023-05-22 18:18:42 +0200

    -Correction de la création des tables -Création de tous les fichiers csv remplis pour insérer des données -Insertion des données dans les tables

--- bdd.sql
+++ bdd.sql
@@ -23,9 +23,9 @@ CREATE TABLE evenement (
     localisation VARCHAR NOT NULL,
     prix INT NOT NULL,
     places INT NOT NULL,
-    passe BOOLEAN NOT NULL,
-    nbr_participants INT
-    CHECK (passe = false AND IS NULL nbr_participants)
+    passe BOOLEAN NOT NULL, --est-ce que l'évenement à déjà eu lieu
+    nbr_participants INT,
+    CHECK ((passe = false AND nbr_participants IS NULL) OR (passe = true AND nbr_participants IS NOT NULL)) --si l'évenement n'a pas encore eu lieu le nombre de participants est inconnue
 );
 
 CREATE TABLE type_user (
@@ -41,24 +41,28 @@ CREATE TABLE utilisateur (
 );
 
 CREATE TABLE genre (
-    nom VARCHAR PRIMARY KEY
+    id_genre SERIAL PRIMARY KEY,
+    nom VARCHAR
 );
 
 CREATE TABLE musique (
     id_musique SERIAL PRIMARY KEY,
+    id_user INT, --le créateur de la musique (ou celui qui l'a posté qu'on suppose être l'auteur)
     titre VARCHAR NOT NULL,
-    duree TIMES NOT NULL,
-    genre VARCHAR NOT NULL,
-    FOREIGN KEY (genre) REFERENCES genre(nom)
+    duree INTERVAL NOT NULL, 
+    id_genre INT,
+    FOREIGN KEY (id_user) REFERENCES utilisateur(id_user),
+    FOREIGN KEY (id_genre) REFERENCES genre(id_genre)
 );
 
 CREATE TABLE playlist (
-    id_playlist SERIAL,
+    id_playlist INT,
     id_user INT,
     id_musique INT,
     PRIMARY KEY (id_playlist, id_user, id_musique),
     FOREIGN KEY (id_user) REFERENCES utilisateur(id_user),
-    FOREIGN KEY (id_musique) REFERENCES musique(id_musique)
+    FOREIGN KEY (id_musique) REFERENCES musique(id_musique),
+    CONSTRAINT unique_musique UNIQUE (id_playlist, id_user, id_musique) --pas plusieurs fois la même musique dans un playlist
 );
 
 CREATE TABLE avis_musique (
@@ -74,12 +78,11 @@ CREATE TABLE avis_musique (
 CREATE TABLE avis_evenement (
     id_auteur INT,
     id_objet INT,
-    note INT NOT NULL,
+    note INT NOT NULL CHECK (note > 0 AND note < 10),
     commentaire VARCHAR,
     PRIMARY KEY (id_auteur, id_objet),
     FOREIGN KEY (id_auteur) REFERENCES utilisateur(id_user),
-    FOREIGN KEY (id_objet) REFERENCES evenement(id_event),
-    CHECK (note > 0 AND note < 10)
+    FOREIGN KEY (id_objet) REFERENCES evenement(id_event)
 );
 
 CREATE TABLE tag (
@@ -95,12 +98,12 @@ CREATE TABLE organisateur (
 ); 
 
 CREATE TABLE sub_genre (
-    parent VARCHAR,
-    enfant VARCHAR,
-    PRIMARY KEY (parent, enfant),
-    FOREIGN KEY (parent) REFERENCES genre(nom),
-    FOREIGN KEY (enfant) REFERENCES genre(nom),
-    CHECK (parent <> enfant)
+    id_parent INT,
+    id_enfant INT,
+    PRIMARY KEY (id_parent, id_enfant),
+    FOREIGN KEY (id_parent) REFERENCES genre(id_genre),
+    FOREIGN KEY (id_enfant) REFERENCES genre(id_genre),
+    CHECK (id_parent <> id_enfant)
 );
 
 CREATE TABLE abonnement (
@@ -117,7 +120,7 @@ CREATE TABLE ami (
     user2 INT,
     PRIMARY KEY (user1, user2),
     FOREIGN KEY (user1) REFERENCES utilisateur(id_user),
-    FOREIGN KEY (user2) REFERENCES utilisateur(id_user),
+    FOREIGN KEY (user2) REFERENCES utilisateur(id_user)
 );
 
 CREATE TABLE membre (
@@ -146,10 +149,10 @@ CREATE TABLE tagged_musique (
 
 CREATE TABLE tagged_genre (
     tag VARCHAR,
-    id_objet VARCHAR,
+    id_objet INT,
     PRIMARY KEY (tag, id_objet),
     FOREIGN KEY (tag) REFERENCES tag(tag_name),
-    FOREIGN KEY (id_objet) REFERENCES genre(nom)
+    FOREIGN KEY (id_objet) REFERENCES genre(id_genre)
 );
 
 CREATE TABLE tagged_evenement (
@@ -160,10 +163,24 @@ CREATE TABLE tagged_evenement (
     FOREIGN KEY (id_objet) REFERENCES evenement(id_event)
 );
 
-CREATE TABLE tagged_user (
-    tag VARCHAR,
-    id_objet INT,
-    PRIMARY KEY (tag, id_objet),
-    FOREIGN KEY (tag) REFERENCES tag(tag_name),
-    FOREIGN KEY (id_objet) REFERENCES utilisateur(id_user)
-);
+
+-- Insertion de données :
+
+\COPY evenement FROM 'csv/evenement.csv' DELIMITER ',' CSV HEADER;
+\COPY type_user FROM 'csv/type_user.csv' DELIMITER ',' CSV HEADER;
+\COPY utilisateur FROM 'csv/utilisateur.csv' DELIMITER ',' CSV HEADER;
+\COPY genre FROM 'csv/genre.csv' DELIMITER ',' CSV HEADER;
+\COPY musique FROM 'csv/musique.csv' DELIMITER ',' CSV HEADER;
+\COPY playlist FROM 'csv/playlist.csv' DELIMITER ',' CSV HEADER;
+\COPY avis_musique FROM 'csv/avis_musique.csv' DELIMITER ',' CSV HEADER;
+\COPY avis_evenement FROM 'csv/avis_evenement.csv' DELIMITER ',' CSV HEADER;
+\COPY tag FROM 'csv/tag.csv' DELIMITER ',' CSV HEADER;
+\COPY organisateur FROM 'csv/organisateur.csv' DELIMITER ',' CSV HEADER;
+\COPY sub_genre FROM 'csv/sub_genre.csv' DELIMITER ',' CSV HEADER;
+\COPY abonnement FROM 'csv/abonnement.csv' DELIMITER ',' CSV HEADER;
+\COPY ami FROM 'csv/ami.csv' DELIMITER ',' CSV HEADER;
+\COPY membre FROM 'csv/membre.csv' DELIMITER ',' CSV HEADER;
+\COPY tagged_user FROM 'csv/tagged_user.csv' DELIMITER ',' CSV HEADER;
+\COPY tagged_musique FROM 'csv/tagged_musique.csv' DELIMITER ',' CSV HEADER;
+\COPY tagged_evenement FROM 'csv/tagged_evenement.csv' DELIMITER ',' CSV HEADER;
+\COPY tagged_genre FROM 'csv/tagged_genre.csv' DELIMITER ',' CSV HEADER;
